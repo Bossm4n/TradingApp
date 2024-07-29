0bbb2efae9ab1f01145035895983d0005fdafb02
y12806ja <bachar.assad@student.manchester.ac.uk>
2024-07-29 15:09:43 +0100

    fixed navbar

--- trading-app/src/components/Navbar.tsx
+++ trading-app/src/components/Navbar.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
-    <div>
-        <Link to={"../webpages/Home.tsx"}></Link>
-        <Link to={"../webpages/login"}></Link>
-        <Link to={"../webpages/trading"}></Link>
-    </div>
-  )
+    <nav>
+      <ul>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/trading">Trading</Link>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
