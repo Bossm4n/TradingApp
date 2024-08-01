9638cfb28a6db4983694c6bea324e6dab46635f8
Basil Osman <m3anbaza21@gmail.com>
2024-08-01 18:29:42 +0100

    Created Assest and User class with simple getter and setter functions for attributes

--- trading-app-backend/src/main/java/com/assadosman/Trading/App/model/Asset.java
+++ trading-app-backend/src/main/java/com/assadosman/Trading/App/model/Asset.java
@@ -1,6 +1,28 @@
 package com.assadosman.Trading.App.model;
 
 public class Asset {
+    private int price;
+    private int marketCap;
 
-    public Asset(){}
+    public Asset(int price, int marketCap) {
+        this.price = price;
+        this.marketCap = marketCap;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getMarketCap() {
+        return marketCap;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setMarketCap(int marketCap) {
+        this.marketCap = marketCap;
+    }
+    
 }
--- trading-app-backend/src/main/java/com/assadosman/Trading/App/model/User.java
+++ trading-app-backend/src/main/java/com/assadosman/Trading/App/model/User.java
@@ -1,6 +1,38 @@
 package com.assadosman.Trading.App.model;
 
 public class User {
+    private String name;
+    private int UserID;
+    private String email;
+    private String hashedPassword;
 
-    public User(){}
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.hashedPassword = hashingFunction(password);
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public String testPassword(String inputtedPassword){
+        // Put password through a hashing function then
+        String hashedInput = hashingFunction(inputtedPassword);
+
+        if(hashedPassword.equals(hashedInput)){
+            return inputtedPassword;
+        }
+        else{
+            return "Incorrect Password";
+        }
+    }
+
+    private String hashingFunction(String password){
+        // Implement a hashing function
+        return password;
+    }
 }
